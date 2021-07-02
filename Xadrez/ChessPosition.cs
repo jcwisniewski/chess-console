2ff5dc51019676bb91af930a67dd5a7fbf7a06b9
jcwisniewski <julio.wisnieski@gmail.com>
2021-07-02 20:19:16 -0300

    Created ChessPosition class

---
+++ Xadrez/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿using Tabuleiro;
+
+namespace Xadrez
+{
+    public class ChessPosition
+    {
+        public char column { get; set; }
+        public int row { get; set; }
+        
+
+        public ChessPosition(char column, int row)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + row;
+        }
+
+    }
+}
